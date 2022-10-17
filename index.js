5a599a5d6241bd7604a1aac79827d975887c9c71
RAKIBUL <rakibulmd933@gmail.com>
2022-10-17 21:00:37 +0600

    [ADD] verify JWT

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2kmaoa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		return res.status(401).send({ message: 'Unauthorized access' });
+	};
+	const token = authHeader.split(' ')[1];
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+		if (err) {
+			return res.status(403).send({ message: 'Forbidden access' })
+		}
+		req.decoded = decoded;
+		next();
+	});
+}
+
 async function run() {
 	try {
 		const servicesCollection = client.db("doctors_portal").collection("services");
@@ -57,10 +72,17 @@ async function run() {
 		});
 
 		// Booking 
-		app.get('/booking', async (req, res) => {
-			const cursor = bookingCollection.find({});
-			const booking = await cursor.toArray();
-			res.send(booking);
+		app.get('/booking', verifyJWT, async (req, res) => {
+			const patient = req.query.patient;
+			const decodedEmail = req.decoded.email;
+			if (patient === decodedEmail) {
+				const query = { patient: patient };
+				const bookings = await bookingCollection.find(query).toArray();
+				return res.send(bookings);
+			}
+			else {
+				return res.status(403).send({ message: 'forbidden access' });
+			}
 		});
 
 		// user added
